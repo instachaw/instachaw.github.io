b73d6dca74f0b32a053dc8b3d81246810b42b50e
creatrixity <creatrixity@gmail.com>
2019-04-22 10:29:30 +0100

    Add normalized entity caching with selectors.

--- src/Store/Store/actions.ts
+++ src/Store/Store/actions.ts
@@ -1,8 +1,13 @@
 'use strict';
+import { normalize } from 'normalizr';
+
 import { Dispatch } from 'redux';
 
 import { ActionTypes } from "@Constants";
-import { IStoresData } from '@Interfaces/Redux/Store';
+import { IStorePage } from '@Interfaces';
+
+import { EntityActions } from '@Store/Entity/actions';
+import { storeSchema } from '@Store/schema';
 
 /**
  * ACTIONS
@@ -17,20 +22,30 @@ export const StoreActions = {
 		type: ActionTypes.Store.ResetReducer
 	}),
 
+	toggleStoresFetchingStatus: (isFetchingStores:boolean) => {
+		return StoreActions.Map({
+			isFetchingStores: !isFetchingStores
+		})
+	},
+
 	fetchStores: () => {
-		return (dispatch:Dispatch, {}:any, api:any) => {
-	
-			dispatch(StoreActions.Map({ isFetchingStores: true }))
+		return (dispatch:Dispatch, getState:any, api:any) => {	
+			dispatch(StoreActions.toggleStoresFetchingStatus(
+					getState().store.isFetchingStores
+				)
+			);
 
 			api.fetchStores()
 				.then(api.checkStatus)
 				.then(api.toJSON)
-        .then(({ stores }:IStoresData) => {
-					dispatch(StoreActions.Map({
-							isFetchingStores: false,
-							stores
-						})
-					)
+        .then(({ stores }:IStorePage.IStoresData) => {
+					const { entities } = normalize(stores, [ storeSchema ]);
+
+					dispatch(StoreActions.toggleStoresFetchingStatus(
+							getState().store.isFetchingStores
+						)
+					);
+					dispatch(EntityActions.Map({ ...entities }))
 				})
         .catch((e:any) => api.errorHandler(dispatch, e))
 		}
