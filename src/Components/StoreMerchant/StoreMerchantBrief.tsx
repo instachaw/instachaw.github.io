944aeaa3f004ff53320a802f44df3535f6c7ac21
creatrixity <creatrixity@gmail.com>
2019-05-23 23:23:19 +0100

    refactor: eliminate duplication across store merchants components

--- src/Components/StoreMerchant/StoreMerchantBrief.tsx
+++ src/Components/StoreMerchant/StoreMerchantBrief.tsx
@@ -13,20 +13,13 @@ type StoreMerchantBriefProps = {};
 export const StoreMerchantBrief:React.FC<StoreMerchantBriefProps> = ({ }) => {
   return (
     <Box padding={`${theme.space[0]} 0`}>
-      <Grid>
-        <Row>
-          <Col>
-            <Heading
-              use="h4"
-              margin={`${theme.space[1]} 0`}
-              color={theme.palette.grayscale[2]}
-              data-testid={'store-merchandise-screen-title'}
-            >
-              Kilimanjaro, Choba
-            </Heading>
-          </Col>
-        </Row>
-      </Grid>
+      <Heading
+        margin={`${theme.space[1]} 0`}
+        color={theme.palette.grayscale[2]}
+        data-testid={'store-merchandise-screen-title'}
+      >
+        Kilimanjaro, Choba
+      </Heading>
     </Box>
   )
 }
