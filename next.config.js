c1324c09dc9110b755de1022ed8964dd009217c0
creatrixity <creatrixity@gmail.com>
2019-06-09 14:06:09 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    feat: compile static assets
    
    # This is the commit message #2:
    
    feat: add environment based api endpoint switching

--- next.config.js
+++ next.config.js
@@ -2,22 +2,39 @@ const withPlugins = require('next-compose-plugins');
 
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
-const withSass = require('@zeit/next-sass');
 const withBundleAnalyzer = require("@zeit/next-bundle-analyzer");
 const nextEnv = require('next-env');
 const dotenvLoad = require('dotenv-load');
 
 dotenvLoad();
 
+const {
+	NODE_ENV,
+	NEXT_PUBLIC_DEVELOPMENT_API_URL,
+	NEXT_PUBLIC_PRODUCTION_API_URL
+} = process.env
+const env = NODE_ENV || 'development'
+
+const envSpecifics = {
+  development: {
+    api: NEXT_PUBLIC_DEVELOPMENT_API_URL,
+  },
+  production: {
+    api: NEXT_PUBLIC_PRODUCTION_API_URL,
+  },
+}[env];
+
 module.exports = withPlugins(
 	[
 		[withTypescript],
 		[withCSS],
-		[withSass],
 		[withBundleAnalyzer],
 		[nextEnv()]
 	],
 	{
+		publicRuntimeConfig: {
+			envSpecifics
+		},
 		analyzeServer: ["server", "both"].includes(process.env.BUNDLE_ANALYZE),
 		analyzeBrowser: ["browser", "both"].includes(process.env.BUNDLE_ANALYZE),
 		bundleAnalyzerConfig: {
@@ -28,6 +45,13 @@ module.exports = withPlugins(
 			browser: {
 				analyzerMode: 'static',
 				reportFilename: '../bundles/client.html'
+			}
+		},
+
+		exportPathMap: function() {
+			return {
+				'/': { page: '/home' },
+				'/stores/index.html': { page: '/stores' }
 			}
 		}
 	}
