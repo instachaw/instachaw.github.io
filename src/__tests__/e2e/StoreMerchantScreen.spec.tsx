b6026eb6c9e72ebb4af7ed8a462da6f0a0e38534
creatrixity <creatrixity@gmail.com>
2019-06-13 09:24:08 +0100

    change tests

--- src/__tests__/e2e/StoreMerchantScreen.spec.tsx
+++ src/__tests__/e2e/StoreMerchantScreen.spec.tsx
@@ -64,6 +64,8 @@ describe('Store Product Page', () => {
         // Load more products...
         const loadStoreBtn = await getByTestId($document, loadStoreBtnProductTestId);
         await loadStoreBtn.click()
+
+        expect(loadStoreBtn.hasAttribute('disabled')).toBeTruthy();
         // Expect new products to have been loaded
         await wait(async () => {
           storeMerchantProducts = await queryAllByTestId($document, storeProductTestId)
