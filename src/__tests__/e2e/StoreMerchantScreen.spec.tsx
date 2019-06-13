856b7d068274f97c32e204beaadbb107579aff63
creatrixity <creatrixity@gmail.com>
2019-06-13 09:29:50 +0100

    add wait interval

--- src/__tests__/e2e/StoreMerchantScreen.spec.tsx
+++ src/__tests__/e2e/StoreMerchantScreen.spec.tsx
@@ -65,7 +65,7 @@ describe('Store Product Page', () => {
         const loadStoreBtn = await getByTestId($document, loadStoreBtnProductTestId);
         await loadStoreBtn.click()
 
-        expect(loadStoreBtn.hasAttribute('disabled')).toBeTruthy();
+        await page.waitFor(3000)
         // Expect new products to have been loaded
         await wait(async () => {
           storeMerchantProducts = await queryAllByTestId($document, storeProductTestId)
