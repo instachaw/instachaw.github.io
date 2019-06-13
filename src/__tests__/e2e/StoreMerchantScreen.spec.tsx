1c2dc65f386f638dc9d7d2ddbbd7d8cee34529f4
instachaw <41760776+instachaw@users.noreply.github.com>
2019-06-13 10:10:34 +0100

    Update StoreMerchantScreen.spec.tsx

--- src/__tests__/e2e/StoreMerchantScreen.spec.tsx
+++ src/__tests__/e2e/StoreMerchantScreen.spec.tsx
@@ -67,8 +67,6 @@ describe('Store Product Page', () => {
 
         console.log(loadStoreBtn.getProperty('innerText'))
 
-        expect(loadStoreBtn.getAttribute('disabled').length).toBeTruthy()
-
         // Expect new products to have been loaded
         await wait(async () => {
           storeMerchantProducts = await queryAllByTestId($document, storeProductTestId)
