e3935753a2946646329daa4e863e14259c918560
instachaw <41760776+instachaw@users.noreply.github.com>
2019-06-13 10:40:52 +0100

    Update StoreMerchantScreen.spec.tsx

--- src/__tests__/e2e/StoreMerchantScreen.spec.tsx
+++ src/__tests__/e2e/StoreMerchantScreen.spec.tsx
@@ -64,11 +64,11 @@ describe('Store Product Page', () => {
         // Load more products...
         const loadStoreBtn = await getByTestId($document, loadStoreBtnProductTestId);
         
+        loadStoreBtn.click()
+
         const textHandle = await loadStoreBtn.getProperty('disabled');
         const text = await textHandle.jsonValue();
         console.log({ text, textHandle })
-
-        loadStoreBtn.click()
 
         // Expect new products to have been loaded
         await wait(async () => {
