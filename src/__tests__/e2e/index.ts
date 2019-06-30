5703c00812f88eb0d09f7af565a9a964e9be302b
creatrixity <creatrixity@gmail.com>
2019-06-30 15:13:31 +0100

    refactor: add log statement

--- src/__tests__/e2e/index.ts
+++ src/__tests__/e2e/index.ts
@@ -6,3 +6,5 @@ export const host = envSpecifics.host;
 export const storeUrl = `${host}/store/genesis-choba-2`;
 export const searchResultsUrl = `${host}/search?q=chicken`;
 export const storesUrl = `${host}/stores`;
+
+console.log({ host })
